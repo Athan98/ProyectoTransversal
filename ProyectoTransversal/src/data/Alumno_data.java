b96dd5e75f251b03e0474599e66098d89d4a16ea
DPontalti <diegopontalti@hotmail.com>
2023-09-18 10:36:34 -0300

    -

--- ProyectoTransversal/src/data/Alumno_data.java
+++ ProyectoTransversal/src/data/Alumno_data.java
@@ -9,8 +9,6 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import org.mariadb.jdbc.Statement;
 
